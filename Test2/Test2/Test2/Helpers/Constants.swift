e9cfabb37bd0831d4ed85398bfa866e3d1810fa2
Lakmali Kaluarachchi <kaluarachchilakmali@gmail.com>
2020-09-22 13:05:35 +0530

    Create Constants.swift

---
+++ Test2/Test2/Test2/Helpers/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Test2
+//
+//  Created by Lionel Rajapakse on 9/20/20.
+//  Copyright © 2020 Lionel Rajapakse. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        
+       static let homeViewController = "HomeViewController"
+    }
+}
